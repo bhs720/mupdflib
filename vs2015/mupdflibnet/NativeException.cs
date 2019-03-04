8f204ac0f40d13b61cc4c7af85d1a2fb0625a23c
Ben Smith <bensmith720@gmail.com>
2019-03-03 20:49:53 -0500

    add NativeException class

---
+++ vs2015/mupdflibnet/NativeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mupdflibnet
+{
+    /// <summary>
+    /// Represents an exception that occurred in unmanaged code
+    /// </summary>
+    public class NativeException : Exception
+    {
+        public NativeException(string message) : base(message) { }
+    }
+}
